21cb44ef636fc4dbf8c62335f381188b3f276985
Aayush Ghimire <aausgh@gmail.com>
2024-08-15 17:05:57 +0545

    first project hello world

---
+++ Awesome01/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  SafeAreaView
+} from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello World!</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
